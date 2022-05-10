48f7c25c236ddeda8863ce35c4bc2fd41a030852
MartinMosqueira <tinchomosqueira@gmail.com>
2022-05-10 11:18:00 -0300

    Finished example facade pattern
    Finished example BankAccount, facade pattern.

--- BankAccount-Facade/src/app/Main.java
+++ BankAccount-Facade/src/app/Main.java
@@ -1,7 +1,11 @@
 package app;
 
+import model.FacadeCashier;
+
 public class Main {
     public static void main(String[] args){
-
+        FacadeCashier cashier = new FacadeCashier();
+        cashier.enter_credentials();
+        cashier.extract_money();
     }
 }
--- BankAccount-Facade/src/model/FacadeCashier.java
+++ BankAccount-Facade/src/model/FacadeCashier.java
@@ -11,5 +11,63 @@ public class FacadeCashier {
         this.account = null;
     }
 
+    public void enter_credentials(){
+        boolean card_correct = this.authentication.read_card();
 
+        if(card_correct){
+
+            String key = this.authentication.enter_key();
+
+            boolean password_correct = this.authentication.check_key(key);
+
+            if(password_correct){
+
+                this.account = this.authentication.get_account();
+
+                return;
+
+            }
+
+        }
+
+        authentication.fail();
+    }
+
+    public void extract_money(){
+        if(this.account != null){
+
+            int quantity = this.cashier.enter_quantity();
+
+            boolean has_binance = this.cashier.has_binance(quantity);
+
+            if(has_binance){
+
+                boolean sufficient_balance = ((int)this.account.check_balance_available()) >= quantity;
+
+                if(sufficient_balance){
+
+                    this.account.block_account();
+
+                    this.account.extract_balance(quantity);
+
+                    this.account.update_account();
+
+                    this.account.unlock_account();
+
+                    this.cashier.send_money();
+
+                    System.out.println(this.cashier.print_ticket());
+
+                }
+
+                else{
+
+                    this.account.fail();
+
+                }
+
+            }
+
+        }
+    }
 }
